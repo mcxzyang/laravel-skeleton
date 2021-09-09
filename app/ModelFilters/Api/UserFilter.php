05c4cfe83970da5b32cae6bb51efde5ee6cd7ff8
cherrybeal <fan.yang@homepartners.tech>
2021-09-09 16:01:26 +0800

    test cs-fixer

---
+++ app/ModelFilters/Api/UserFilter.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\ModelFilters\Api;
+
+use EloquentFilter\ModelFilter;
+
+class UserFilter extends ModelFilter
+{
+    /**
+    * Related Models that have ModelFilters as well as the method on the ModelFilter
+    * As [relationMethod => [input_key1, input_key2]].
+    *
+    * @var array
+    */
+    public $relations = [
+
+    ];
+}
